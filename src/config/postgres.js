cb3b1a13e8476a226f5ba1f2819b7e728b0f1c36
Johan Sing <johan.singainy-moutiapin@ynov.com>
2021-07-19 19:40:25 +0200

    fix: Lint code on postgres.js file / Remove db connexion on index.js

--- src/config/postgres.js
+++ src/config/postgres.js
@@ -1,32 +1,30 @@
 const { Pool } = require('pg');
 const logger = require('./logger');
 
-
 const config = {
-    host : process.env.POSTGRES_HOST,
-    user : process.POSTGRES_USERNAME,
-    password : process.env.POSTGRES_PWD,
-    database : process.env.POSTGRES_DBNAME,
-    port : process.env.POSTGRES_PORT,
-    dialect : process.env.POSTGRES_DIALECT,
-    keepAlive : true
-}
-
+  host: process.env.POSTGRES_HOST,
+  user: process.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PWD,
+  database: process.env.POSTGRES_DBNAME,
+  port: process.env.POSTGRES_PORT,
+  dialect: process.env.POSTGRES_DIALECT,
+  keepAlive: true,
+};
 
 const client = new Pool(config);
 
-client.connect()
-.then(_ => {
+client
+  .connect()
+  .then(() => {
     logger.info(`Connected to Postgres !
     ==================================
     Host : ${process.env.POSTGRES_HOST}
     Database : ${process.env.POSTGRES_DBNAME}
     ==================================
     `);
-})
-.catch(error => {
+  })
+  .catch((error) => {
     logger.error(error.message);
-})
-
+  });
 
 module.export = client;
