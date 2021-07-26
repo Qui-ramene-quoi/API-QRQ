db6916a964f462cab4c82d2594e848d70d0f2734
Johan Sing <johan.singainy-moutiapin@ynov.com>
2021-07-27 01:46:15 +0200

    feat: Create validators to validate data in request to create user

---
+++ src/app/user/middlewares/validators/CreateUserValidator.js
@@ -0,0 +1,64 @@
+const joi = require('joi');
+const JoiPhoneNumber = joi.extend(require('joi-phone-number'));
+const Response = require('../../../core/response');
+
+const firstStep = joi.object({
+  phoneNumber: JoiPhoneNumber.string()
+    .phoneNumber({
+      defaultCountry: 'FR',
+      format: 'international',
+      strict: true,
+    })
+    .required(),
+});
+
+const secondStep = joi.object({
+  phoneNumber: JoiPhoneNumber.string()
+    .phoneNumber({
+      defaultCountry: 'FR',
+      format: 'national',
+    })
+    .required(),
+  tokenConfirm: joi.string().hex().required(),
+  otp: joi.number().required(),
+});
+
+const CreateUserValidator = async (req, res, next) => {
+  if (
+    req.body.phoneNumber !== undefined &&
+    req.body.tokenConfirm === undefined &&
+    req.body.otp === undefined
+  ) {
+    try {
+      const result = await firstStep.validateAsync({
+        phoneNumber: req.body.phoneNumber,
+      });
+      res.locals.user = result;
+      res.locals.step = true;
+      next();
+    } catch (err) {
+      res.status(400);
+      res.send(Response(res.statusCode, 'user.validation_failed', err.message));
+    }
+  } else if (
+    req.body.phoneNumber !== undefined &&
+    req.body.tokenConfirm !== undefined &&
+    req.body.otp !== undefined
+  ) {
+    try {
+      const result = await secondStep.validateAsync({
+        phoneNumber: req.body.phoneNumber,
+        tokenConfirm: req.body.tokenConfirm,
+        otp: req.body.otp,
+      });
+      res.locals.user = result;
+      res.locals.step = false;
+      next();
+    } catch (err) {
+      res.status(400);
+      res.send(Response(res.statusCode, 'user.validation_failed', err.message));
+    }
+  }
+};
+
+module.exports = CreateUserValidator;
