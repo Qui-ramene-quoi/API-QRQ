7f003b63119b39b9bc8d661bd0d839f717636dcc
Johan Sing <johan.singainy-moutiapin@ynov.com>
2021-07-27 18:28:21 +0200

    feat: Create lot of methods for JWT (create, verify and decode)

---
+++ src/app/authentication/services/JWTService.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const path = require('path');
+const jwt = require('jsonwebtoken');
+
+const privateKey = fs.readFileSync(
+  path.join(__dirname, '../../../config/jwt/private.key'),
+  {
+    encoding: 'utf8',
+  },
+);
+const publicKey = fs.readFileSync(
+  path.join(__dirname, '../../../config/jwt/public.key'),
+  {
+    encoding: 'utf8',
+  },
+);
+
+const i = 'Qui ramène quoi ? (QRQ)';
+const s = 'contact@qrq.com';
+const a = 'https://qrq.com/';
+
+const CreateJWT = (payload) => {
+  const signOptions = {
+    issuer: i,
+    subject: s,
+    audience: a,
+    expiresIn: 60 * 60 * 24 * 15,
+    algorithm: 'RS256',
+  };
+
+  return jwt.sign(payload, privateKey, signOptions);
+};
+
+const VerifyJWT = (token) => {
+  const verifyOptions = {
+    issuer: i,
+    subject: s,
+    audience: a,
+    expiresIn: 60 * 60 * 24 * 15,
+    algorithm: ['RS256'],
+  };
+
+  try {
+    return jwt.verify(token, publicKey, verifyOptions);
+  } catch (err) {
+    return false;
+  }
+};
+
+const DecodeJWT = (token) => jwt.decode(token, { complete: true });
+
+module.exports = { CreateJWT, VerifyJWT, DecodeJWT };
