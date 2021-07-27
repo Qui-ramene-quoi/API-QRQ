dec05eaafec3e0678e2822054a88ffdd4da20ff3
Johan Sing <johan.singainy-moutiapin@ynov.com>
2021-07-27 16:08:38 +0200

    feat: Implemente logical to create user in controller

--- src/app/user/controllers/user.js
+++ src/app/user/controllers/user.js
@@ -1,5 +1,6 @@
 const UserServiceClass = require('../service/user');
-const userRepo = require('../repo/userRepo');
+const userRepo = require('../repo/user');
+const Response = require('../../core/response');
 
 const userTable = new UserServiceClass(userRepo);
 
@@ -31,17 +32,82 @@ userController.prototype.getUser = async (req, res) => {
   }
 };
 
-userController.prototype.insertUser = async (req, res) => {
+userController.prototype.createUser = async (req, res, next) => {
   this.query = null;
-  const input = req.body;
-  try {
-    this.query = await userTable.insert(input);
-    res.status(201).json({
-      code: 201,
-      user: this.query,
-    });
-  } catch (e) {
-    res.send(e.message);
+  const requestBody = res.locals.user;
+
+  if (res.locals.step) {
+    this.query = await userTable.findByPhone(requestBody.phoneNumber);
+
+    if (this.query.length === 0) {
+      try {
+        this.query = await userTable.insert(requestBody);
+        // TODO
+        // Send OTP on phone number user
+
+        res.status(428);
+        res.send(
+          Response(res.statusCode, 'user.precondition_required', {
+            message: 'The request must be resent with the OTP received by SMS.',
+            data: { tokenConfirm: this.query[0].token_confirm },
+          })
+        );
+      } catch (err) {
+        res.status(500);
+        res.send(
+          Response(
+            res.statusCode,
+            'user.internal_server_error',
+            'An error occured, please retry later.',
+          )
+        );
+      }
+    } else if (
+      this.query[0].token_confirm === null &&
+      this.query[0].confirmed === true
+    ) {
+      res.status(409);
+      res.send(
+        Response(
+          res.statusCode,
+          'user.phone_number_already_exist',
+          'Phone number is already take by another user.',
+        )
+      );
+    } else {
+      // TODO
+      // Create and send new OTP
+
+      res.status(428);
+      res.send(
+        Response(res.statusCode, 'user.precondition_required', {
+          message: 'The request must be resent with the OTP received by SMS.',
+          data: { tokenConfirm: this.query[0].token_confirm },
+        })
+      );
+    }
+  } else {
+    this.query = await userTable.findByPhoneNumberAndTokenConfirm(
+      requestBody.phoneNumber,
+      requestBody.tokenConfirm,
+    );
+
+    if (this.query.length === 0) {
+      res.status(404);
+      res.send(Response(res.statusCode, 'user.not_found', 'User not found.'));
+    } else {
+      // TODO
+      // Verify OTP to validate phone number
+
+      this.query = await userTable.confirmUser(this.query[0].id);
+      // TODO
+      // Create JWT and save it
+
+      res.status(201);
+      res.locals.authentication = '9278932982987SYUISIUAIAEG';
+      [res.locals.user] = this.query;
+      next();
+    }
   }
 };
 
