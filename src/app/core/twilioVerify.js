39f2f07580ed69fa886f25de8dced420c57429b0
Johan Sing <johan.singainy-moutiapin@ynov.com>
2021-08-03 17:17:49 +0200

    fix: Update implementation of Twilio methods. Transform it by async/await methods

--- src/app/core/twilioVerify.js
+++ src/app/core/twilioVerify.js
@@ -3,26 +3,38 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const verifyService = process.env.TWILIO_SERVICE;
 const client = require('twilio')(accountSid, authToken);
 
-const sendSMSVerify = (phoneNumber) => {
-  client.verify
-    .services(verifyService)
-    .verifications.create({ to: phoneNumber, channel: 'sms' })
-    .then((verification) => {
-      if (verification.sid === undefined) {
-        throw new Error('OTP not send.');
-      }
-    });
+const sendSMSVerify = async (phoneNumber) => {
+  let verification = null;
+
+  try {
+    verification = await client.verify
+      .services(verifyService)
+      .verifications.create({ to: phoneNumber, channel: 'sms' });
+  } catch (err) {
+    throw new Error('authentication.unauthorized.internal_error_server');
+  }
+
+  if (verification.sid === undefined) {
+    throw new Error('authentication.unauthorized.otp_not_send');
+  }
 };
 
-const verifyCode = (phoneNumber, code) => {
-  client.verify
-    .services(verifyService)
-    .verificationChecks.create({ to: phoneNumber, code })
-    .then((verificationCheck) => {
-      if (verificationCheck.status !== 'approved') {
-        throw new Error('OTP is not valid.');
-      }
-    });
+const verifyCode = async (phoneNumber, code) => {
+  let verificationCheck = null;
+
+  try {
+    verificationCheck = await client.verify
+      .services(verifyService)
+      .verificationChecks.create({ to: phoneNumber, code });
+  } catch (error) {
+    throw new Error('authentication.unauthorized.internal_error_server');
+  }
+
+  if (verificationCheck.status !== 'approved') {
+    throw new Error('authentication.unauthorized.otp_invalid');
+  } else {
+    return true;
+  }
 };
 
 module.exports = { sendSMSVerify, verifyCode };
