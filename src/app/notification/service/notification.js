7eb7ede54d2d555367d67ac5e8027aeb303c999d
Johan Sing <johan.singainy-moutiapin@ynov.com>
2021-09-09 17:51:02 +0200

    feat: Create repo and service for notification table database

---
+++ src/app/notification/service/notification.js
@@ -0,0 +1,77 @@
+class NotificationService {
+  constructor(repo) {
+    this.repo = repo;
+  }
+
+  async findById(id) {
+    let query = null;
+    try {
+      query = await this.repo.findById(id);
+      return query.rows;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async findByUserId(userId) {
+    let query = null;
+    try {
+      query = await this.repo.findByUserId(userId);
+      return query.rows;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async findAllReaded() {
+    let query = null;
+    try {
+      query = await this.repo.findAllReaded();
+      return query.rows;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async findAllUnreaded() {
+    let query = null;
+    try {
+      query = await this.repo.findAllUnreaded();
+      return query.rows;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async insert(notification) {
+    let query = null;
+    try {
+      query = await this.repo.insert(notification);
+      return query.rows;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async readNotification(id) {
+    let query = null;
+    try {
+      query = await this.repo.readNotification(id);
+      return query.rows;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  async unreadNotification(id) {
+    let query = null;
+    try {
+      query = await this.repo.unreadNotification(id);
+      return query.rows;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+}
+
+module.exports = NotificationService;
